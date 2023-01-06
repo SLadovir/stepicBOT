7bb33d4ecb821fe281bd0709b43907f9b33b26ba
SLD/VR <sladovir@mail.ru>
2023-01-06 14:21:04 +0300

    ШЩХЪЫЯСТЬ. добавил ввод имени

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def print_hi(name: str) -> None:
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -10,5 +5,7 @@ def print_hi(name: str) -> None:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    name = input("Введите свое имя: ")
+    print_hi(name)
+
 
