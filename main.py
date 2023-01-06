7662f32e71812d0e2a54d302f1af8f91819c77d6
SLD/VR <sladovir@mail.ru>
2023-01-06 15:58:22 +0300

    some change main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from dataclasses import dataclass
+
 
 def print_hi(name: str) -> None:
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
+@dataclass
+class User:
+    user_id: int
+    name: str
+    age: int
+    email: str
+
+
 if __name__ == '__main__':
-    name = input("Введите свое имя: ")
-    print_hi(name)
+    user_1: User = User(42, 'Vasiliy', 23, 'vasya_pupkin@pochta.ru')
+    print(user_1)
 
 
