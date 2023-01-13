eee214ea79e186f826c8185162d98b7440f404ed
SLD/VR <sladovir@mail.ru>
2023-01-13 20:12:24 +0300

    Revert "Кривая реализация бота скачивающего видос с ютуба (надо бд добавить+удалять с сервера, чтобы хранилось все на сервере Тг) + ловить ссылки с ютуба"
    This reverts commit 7c78c965d5e307cad5cb4941e65248ad44783db0.

--- smart_start_Bot.py
+++ smart_start_Bot.py
@@ -1,4 +1,3 @@
-import pytube
 import requests
 from aiogram import Bot, Dispatcher, executor, types
 
@@ -56,23 +55,6 @@ async def send_photo_echo(message: types.Message):
 @dp.message_handler(content_types=['sticker'])
 async def send_sticker_echo(message: types.Message):
     await message.answer_sticker(message.sticker.file_id)
-
-
-@dp.message_handler()
-async def process_help_command(message: types.Message):
-    print('Кто-то хочет Ютубчек')
-    file_path: str = ''
-    if message.text.find('youtube.com'):
-        video_link = message.text
-        yt = pytube.YouTube(video_link)
-        i_tag = 22
-        print('Предподготовка')
-        stream = yt.streams.get_by_itag(i_tag)
-        print('Скачивание')
-        file_path = stream.download()
-        print('Отправка')
-        await bot.send_video(message.chat.id, open(file_path[41:], 'rb'))
-    await message.reply(message.text)
 
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения, кроме команд "/start" и "/help"
