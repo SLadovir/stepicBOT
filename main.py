7c78c965d5e307cad5cb4941e65248ad44783db0
SLD/VR <sladovir@mail.ru>
2023-01-13 20:10:38 +0300

    Кривая реализация бота скачивающего видос с ютуба (надо бд добавить+удалять с сервера, чтобы хранилось все на сервере Тг) + ловить ссылки с ютуба + можно забахать Так, чтобы он когда качал сторисы с инсты - автоматом грузил их в группу (muzlo_gang) + мб создать канал в который будут загружаться все видосы с Ютуба, которые скачиваются с помощью бота (+ давать определенное количество скачиваний видосов в день)

--- main.py
+++ main.py
@@ -11,66 +11,9 @@ dp: Dispatcher = Dispatcher(bot)
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
+    print(3133)
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
-# Этот хэндлер будет срабатывать на команду "/help"
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.answer('Напиши мне что-нибудь и в ответ я пришлю тебе твое сообщение')
-
-
-@dp.message_handler(commands=['cat'])
-async def process_help_command(message: types.Message):
-    API_PHOTO_URL: str = 'https://aws.random.cat/meow'
-    NAME: str = "котик"
-    response: requests.Response = requests.get(API_PHOTO_URL)
-    if response.status_code == 200:
-        photo_link = response.json()['file']
-        await message.answer_photo(photo=photo_link)
-    else:
-        await message.answer(f'Тут должен был быть {NAME}')
-
-
-@dp.message_handler(commands=['dog'])
-async def process_help_command(message: types.Message):
-    API_PHOTO_URL: str = 'https://random.dog/woof.json'
-    NAME: str = "Собачка"
-    response: requests.Response = requests.get(API_PHOTO_URL)
-    if response.status_code == 200:
-        print('что-то случилось')
-        video_link = response.json()['url']
-        await message.answer_video(video=video_link)
-    else:
-        await message.answer(f'Тут должен был быть {NAME}')
-
-
-# Этот хэндлер будет срабатывать на отправку фото
-@dp.message_handler(content_types=['photo'])
-async def send_photo_echo(message: types.Message):
-    await message.answer_photo(message.photo[0].file_id)
-
-
-# Этот хэндлер будет срабатывать на отправку стикеров
-@dp.message_handler(content_types=['sticker'])
-async def send_sticker_echo(message: types.Message):
-    await message.answer_sticker(message.sticker.file_id)
-
-
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения, кроме команд "/start" и "/help"
-@dp.message_handler()
-async def send_echo(message: types.Message):
-    await message.reply(message.text)
-
-
-@dp.message_handler()
-async def process_any_update(message: types.Message):
-    # Выводим апдейт в терминал
-    print(message)
-    # Отправляем сообщение в чат, откуда пришел апдейт
-    await message.answer(text='Вы что-то прислали')
-
-
 if __name__ == '__main__':
-    print('Поехали')
     executor.start_polling(dp, skip_updates=True)
