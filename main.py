1864567f21e05764c8077976bb6e1f63908883be
SLD/VR <sladovir@mail.ru>
2023-01-15 06:57:25 +0300

    Храним секреты) Тсс!..

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import requests
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN: str = '1898674417:AAHvwZRAPg1VLDauHToD10pMHELU_FhxSYU'
+from environs import Env
+
+
+# Создаем экземпляр класса Env и Методом read_env() читаем файл .env и загружаем из него переменные в окружение
+env = Env()
+env.read_env()
+
+BOT_TOKEN: str = env('BOT_TOKEN')   # Сохраняем значение переменной окружения в переменную bot_token
+admin_id = env.int('ADMIN_ID')   # Преобразуем значение переменной окружения к типу int
+
+print(BOT_TOKEN)
+print(admin_id)
 
 # Создаем объекты бота и диспетчера
-bot: Bot = Bot(token=API_TOKEN)
+bot: Bot = Bot(token=BOT_TOKEN)
 dp: Dispatcher = Dispatcher(bot)
 
 
@@ -12,63 +22,6 @@ dp: Dispatcher = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
-
-
-# Этот хэндлер будет срабатывать на команду "/help"
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.answer('Напиши мне что-нибудь и в ответ я пришлю тебе твое сообщение')
-
-
-@dp.message_handler(commands=['cat'])
-async def process_help_command(message: types.Message):
-    API_PHOTO_URL: str = 'https://aws.random.cat/meow'
-    NAME: str = "котик"
-    response: requests.Response = requests.get(API_PHOTO_URL)
-    if response.status_code == 200:
-        photo_link = response.json()['file']
-        await message.answer_photo(photo=photo_link)
-    else:
-        await message.answer(f'Тут должен был быть {NAME}')
-
-
-@dp.message_handler(commands=['dog'])
-async def process_help_command(message: types.Message):
-    API_PHOTO_URL: str = 'https://random.dog/woof.json'
-    NAME: str = "Собачка"
-    response: requests.Response = requests.get(API_PHOTO_URL)
-    if response.status_code == 200:
-        print('что-то случилось')
-        video_link = response.json()['url']
-        await message.answer_video(video=video_link)
-    else:
-        await message.answer(f'Тут должен был быть {NAME}')
-
-
-# Этот хэндлер будет срабатывать на отправку фото
-@dp.message_handler(content_types=['photo'])
-async def send_photo_echo(message: types.Message):
-    await message.answer_photo(message.photo[0].file_id)
-
-
-# Этот хэндлер будет срабатывать на отправку стикеров
-@dp.message_handler(content_types=['sticker'])
-async def send_sticker_echo(message: types.Message):
-    await message.answer_sticker(message.sticker.file_id)
-
-
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения, кроме команд "/start" и "/help"
-@dp.message_handler()
-async def send_echo(message: types.Message):
-    await message.reply(message.text)
-
-
-@dp.message_handler()
-async def process_any_update(message: types.Message):
-    # Выводим апдейт в терминал
-    print(message)
-    # Отправляем сообщение в чат, откуда пришел апдейт
-    await message.answer(text='Вы что-то прислали')
 
 
 if __name__ == '__main__':
