4e40017582c7a27624fdb86d613193216d7e7cd2
SLD/VR <sladovir@mail.ru>
2023-01-28 02:43:49 +0300

    реворки, кнопки

--- smart_start_Bot.py
+++ smart_start_Bot.py
@@ -2,8 +2,13 @@
 import requests
 from aiogram import Bot, Dispatcher, executor, types
 from config_data.config import load_config
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton, WebAppInfo
 
+BUTTON_1: str = 'Пришли собачек 🦮'
+BUTTON_2: str = 'Жду котиков 🐈'
+but1 = 'dog'
+but2 = 'cat'
+field_placeholder = 'Что ты хочешь?)'
 config = load_config('config_data/.env')
 
 BOT_TOKEN: str = config.tg_bot.token   # Сохраняем значение переменной окружения в переменную bot_token
@@ -12,29 +17,25 @@ bot: Bot = Bot(token=BOT_TOKEN)
 dp: Dispatcher = Dispatcher(bot)
 
 # Создаем объект клавиатуры
-keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup()
+keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(resize_keyboard=True, input_field_placeholder=field_placeholder)
 
 # Создаем объекты кнопок
-button_1: KeyboardButton = KeyboardButton('Собачек 🦮')
-button_2: KeyboardButton = KeyboardButton('Котиков 🐈')
+button_1: KeyboardButton = KeyboardButton(f'{BUTTON_1}')
+button_2: KeyboardButton = KeyboardButton(f'{BUTTON_2}')
 
 keyboard.add(button_1, button_2)
+keyboard.add(KeyboardButton(text='Start Web App', web_app=WebAppInfo(url="https://stepik.org/")))
 
 
-# Этот хэндлер будет срабатывать на команду "/start"
-@dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь', reply_markup=keyboard)
 
 
-# Этот хэндлер будет срабатывать на команду "/help"
-@dp.message_handler(commands=['help'])
 async def process_help_command(message: types.Message):
     await message.answer('Напиши мне что-нибудь и в ответ я пришлю тебе твое сообщение')
 
 
-@dp.message_handler(commands=['cat'])
-async def process_help_command(message: types.Message):
+async def send_cat(message: types.Message):
     API_PHOTO_URL: str = 'https://aws.random.cat/meow'
     NAME: str = "котик"
     response: requests.Response = requests.get(API_PHOTO_URL)
@@ -45,27 +46,26 @@ async def process_help_command(message: types.Message):
         await message.answer(f'Тут должен был быть {NAME}')
 
 
-@dp.message_handler(commands=['dog'])
-async def process_help_command(message: types.Message):
+async def send_dog(message: types.Message):
     API_PHOTO_URL: str = 'https://random.dog/woof.json'
     NAME: str = "Собачка"
     response: requests.Response = requests.get(API_PHOTO_URL)
     if response.status_code == 200:
-        print('что-то случилось')
         video_link: str = response.json()['url']
-        await message.answer_video(video=video_link)
+        if video_link.find('jpg'):
+            await message.answer_photo(photo=video_link)
+        else:
+            await message.answer_video(video=video_link)
     else:
         await message.answer(f'Тут должен был быть {NAME}')
 
 
 # Этот хэндлер будет срабатывать на отправку фото
-@dp.message_handler(content_types=['photo'])
 async def send_photo_echo(message: types.Message):
     await message.answer_photo(message.photo[0].file_id)
 
 
 # Этот хэндлер будет срабатывать на отправку стикеров
-@dp.message_handler(content_types=['sticker'])
 async def send_sticker_echo(message: types.Message):
     await message.answer_sticker(message.sticker.file_id)
 
@@ -88,9 +88,16 @@ async def send_sticker_echo(message: types.Message):
 #     await message.reply(message.text)
 
 
-@dp.message_handler()
 async def send_echo(message: types.Message):
     await message.reply(message.text)
+
+dp.register_message_handler(process_start_command, commands='start')
+dp.register_message_handler(process_help_command, commands='help')
+dp.register_message_handler(send_dog, text=[BUTTON_1, but1])
+dp.register_message_handler(send_cat, text=[BUTTON_2, but2])
+dp.register_message_handler(send_photo_echo, content_types=['photo'])
+dp.register_message_handler(send_sticker_echo, content_types=['sticker'])
+dp.register_message_handler(send_echo)
 
 
 if __name__ == '__main__':
